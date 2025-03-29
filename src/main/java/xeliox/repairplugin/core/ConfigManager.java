67f40ce01aa326587a9b360e90f7a1ef0cb84d26
xEliiox <pichicenturion@gmail.com>
2025-03-29 19:57:43 -0300

    1.1.0

--- src/main/java/xeliox/repairplugin/core/ConfigManager.java
+++ src/main/java/xeliox/repairplugin/core/ConfigManager.java
@@ -1,54 +1,48 @@
 package xeliox.repairplugin.core;
 
+import org.jetbrains.annotations.NotNull;
 import org.simpleyaml.configuration.file.YamlFile;
 import xeliox.repairplugin.RepairPlugin;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 import java.util.logging.Logger;
 
 public class ConfigManager {
-    private final RepairPlugin plugin;
-    private final YamlFile config;
+    private YamlFile config;
     private boolean anvilInteraction;
     private int experienceCost;
     private int experienceCostAll;
 
-    public ConfigManager(RepairPlugin plugin, Logger logger, YamlFile config) {
-        this.plugin = plugin;
-        this.config = config;
-        File configFile = new File(plugin.getDataFolder(), "config.yml");
+    public ConfigManager(@NotNull RepairPlugin plugin, Logger logger) throws IOException {
+        File dataFolder = plugin.getDataFolder();
+        File configFile = new File(dataFolder, "config.yml");
 
-        if (!configFile.exists()) {
-            try {
-                logger.warning("The configuration file does not exist!");
-                logger.info("Creating a new configuration file...");
-
-                InputStream defaultConfigStream = plugin.getResource("config.yml");
-                if (defaultConfigStream != null) {
-                    Files.copy(defaultConfigStream, configFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                    logger.info("Default configuration file copied successfully.");
-                } else {
-                    logger.info("Default configuration file not found in the JAR!");
-                }
-
-            } catch (IOException e) {
-                logger.severe("Failed to create config.yml file: " + e.getMessage());
-            }
+        if (!dataFolder.exists() && !dataFolder.mkdirs()) {
+            logger.severe("Failed to create plugin folder!");
+            return;
         }
 
+        if (!configFile.exists() || configFile.length() == 0) {
+            plugin.saveResource("config.yml", false);
+        }
+
+        config = new YamlFile(configFile);
+
+        try {
+            config.load();
+        } catch (Exception e) {
+            logger.severe("Failed to load config.yml: " + e.getMessage());
+        }
 
         loadConfiguration();
     }
 
-    public void loadConfiguration() {
+    public void loadConfiguration() throws IOException {
 
-        anvilInteraction = getOrSetDefault(config,"Settings.disableAnvilInteraction", false);
-        experienceCost = getOrSetDefault(config,"Settings.experience_cost", 10);
-        experienceCostAll = getOrSetDefault(config,"Settings.experience_cost_all", 15);
+        anvilInteraction = getOrSetDefault("Settings.disableAnvilInteraction", false);
+        experienceCost = getOrSetDefault("Settings.experience_cost", 10);
+        experienceCostAll = getOrSetDefault("Settings.experience_cost_all", 15);
 
         boolean saveRequired = false;
 
@@ -61,12 +55,12 @@ public class ConfigManager {
         }
 
         if (saveRequired) {
-            plugin.saveConfig();
+            config.save();
         }
     }
 
     @SuppressWarnings("unchecked")
-    private <T> T getOrSetDefault(YamlFile config, String path, T defaultValue) {
+    private <T> T getOrSetDefault(String path, T defaultValue) {
         if (!config.contains(path)) {
             config.set(path, defaultValue);
             return defaultValue;
