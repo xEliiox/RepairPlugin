8a76de356ced1059d2a18bb94cc6ccfbb8f0fa69
xEliiox <pichicenturion@gmail.com>
2025-05-02 00:49:31 -0300

    1.1.1

--- src/main/java/xeliox/repairplugin/core/ConfigManager.java
+++ src/main/java/xeliox/repairplugin/core/ConfigManager.java
@@ -1,5 +1,6 @@
 package xeliox.repairplugin.core;
 
+import org.bukkit.Bukkit;
 import org.jetbrains.annotations.NotNull;
 import org.simpleyaml.configuration.file.YamlFile;
 import xeliox.repairplugin.RepairPlugin;
@@ -11,8 +12,10 @@ import java.util.logging.Logger;
 public class ConfigManager {
     private YamlFile config;
     private boolean anvilInteraction;
+    private boolean eventAnvilInteraction;
     private int experienceCost;
     private int experienceCostAll;
+    private int anvilExperienceCost;
 
     public ConfigManager(@NotNull RepairPlugin plugin, Logger logger) throws IOException {
         File dataFolder = plugin.getDataFolder();
@@ -35,41 +38,63 @@ public class ConfigManager {
             logger.severe("Failed to load config.yml: " + e.getMessage());
         }
 
-        loadConfiguration();
+        loadConfiguration(config);
     }
 
-    public void loadConfiguration() throws IOException {
+    public void loadConfiguration(YamlFile config) throws IOException {
 
-        anvilInteraction = getOrSetDefault("Settings.disableAnvilInteraction", false);
-        experienceCost = getOrSetDefault("Settings.experience_cost", 10);
-        experienceCostAll = getOrSetDefault("Settings.experience_cost_all", 15);
+        boolean[] saveRequired = new boolean[]{false};
 
-        boolean saveRequired = false;
+        anvilInteraction = getOrSetDefault("Settings.disableAnvilInteraction", false, saveRequired);
+        eventAnvilInteraction = getOrSetDefault("Settings.cancelEventAnvilInteraction", true,saveRequired);
+        experienceCost = getIntOrDefault("Settings.experience_cost", 10,saveRequired);
+        experienceCostAll = getIntOrDefault("Settings.experience_cost_all", 15,saveRequired);
+        anvilExperienceCost = getIntOrDefault("Settings.anvil_experience_cost", 10,saveRequired);
 
         for (Messages message : Messages.values()) {
             if (!config.contains(message.path)) {
                 config.set(message.path, message.defaultMessage);
-                saveRequired = true;
+                saveRequired[0] = true;
             }
             message.loadMessage(config);
         }
 
-        if (saveRequired) {
+        if (saveRequired[0]) {
             config.save();
         }
     }
 
     @SuppressWarnings("unchecked")
-    private <T> T getOrSetDefault(String path, T defaultValue) {
+    private <T> T getOrSetDefault(String path, T defaultValue, boolean[] saveFlag) {
         if (!config.contains(path)) {
             config.set(path, defaultValue);
+            saveFlag[0] = true;
             return defaultValue;
         }
         return (T) config.get(path);
     }
 
+
+    private int getIntOrDefault(String path, int defaultValue, boolean[] saveFlag) {
+        if (!config.contains(path)) {
+            config.set(path, defaultValue);
+            saveFlag[0] = true;
+            return defaultValue;
+        }
+        Object value = config.get(path);
+        if (value instanceof Number) {
+            return ((Number) value).intValue();
+        }
+        return defaultValue;
+    }
+
+
     public boolean isDisableAnvilInteraction() {
         return anvilInteraction;
+    }
+
+    public boolean isEventAnvilInteraction() {
+        return eventAnvilInteraction;
     }
 
     public int getExperienceCost() {
@@ -78,6 +103,20 @@ public class ConfigManager {
 
     public int getExperienceCostAll() {
         return experienceCostAll;
+    }
+
+    public int getAnvilExperienceCost() {
+        return anvilExperienceCost;
+    }
+
+    public void reloadConfig() throws IOException {
+        try {
+            this.config.load();
+            loadConfiguration(this.config);
+        } catch (Exception e) {
+            Bukkit.getLogger().severe("Error reloading configuration: " + e.getMessage());
+            throw new IOException("Could not reload configuration", e);
+        }
     }
 }
 
